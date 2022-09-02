ad8c0f3c87558015712213d60cfb459748144a38
Kimber Herron <kh582013@gmail.com>
2022-09-02 11:29:01 -0400

    commit tree e9f26d22c9bfb17346068a09c5c18190649cf122

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+"Hello!".toUpperCase(); // 'HELLO!'
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+"Hello".toLowerCase();
+
+const hello = "spy"
+ function logShout(string) {
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+const goSay = "I can't hear you!";
+const yesAgree = "YES INDEED!";
+const loveYes = "I would love to!";
+if (string === string.toLowerCase())  {
+    return goSay;
+}
+ if (string === string.toUpperCase()) {
+    return yesAgree;
+}
+ if ("Let's have dinner together!" === string) {
+    return loveYes; 
+}
+}
